b369bea883fed14b9e218e5bb41ffed8df62c51f
Aditya Borse <57841103+aditya-c137@users.noreply.github.com>
2020-02-12 15:16:31 +0530

    Create Keypad.java

---
+++ JAVA/Assignment2/Keypad.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Key {
+
+}
+
+public class Keypad {
+        public static void main(String[] args){
+                Hashtable<Integer, LinkedList<String>> keys  = new Hashtable<Integer, LinkedList<String>>();
+        }
+}
